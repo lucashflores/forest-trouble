114855b3eb22a78ecba09f4800524e26899b28a0
lucashflores <lucas.henrique.flores2001@gmail.com>
2023-07-26 10:56:23 -0300

    Initial Commit

---
+++ ForestTrouble/ForestTroubleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestTroubleApp.swift
+//  ForestTrouble
+//
+//  Created by Lucas Flores on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ForestTroubleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
